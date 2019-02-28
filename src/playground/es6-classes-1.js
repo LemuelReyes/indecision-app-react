67d654ab1437338ca7dc9dd360862ec9f1e933ea
Lemuel Reyes <reyeslemuel@hotmail.com>
2019-02-27 19:24:31 -0800

    exercises in extends, supers to call parent class and methods, overriding methods from parent class

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,4 +1,6 @@
+// Exercises:
 // classes, constructors, instances, template string concatenation
+// extends, supers to call parent class and methods, overriding methods from parent class
 
 class Person {
     constructor(name = 'Anonymous', age = 0) {
@@ -9,12 +11,52 @@ class Person {
         return `Hi, my name is ${this.name}!`;
     }
     getDescription() {
-        return `${this.name} is ${this.age}(s) old.`
+        return `${this.name} is ${this.age} year(s) old.`
     }
 }
 
-const me = new Person('Lemuel Reyes', 29);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
 
-const other = new Person();
-console.log(other.getDescription());
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+
+        return description;
+    }   
+}
+
+class Traveler extends Person {
+    constructor(name, homeLocation) {
+        super(name);
+        this.homeLocation = homeLocation;
+    }
+    hasHome() {
+        return !!this.homeLocation;
+    }
+    getGreeting(){
+        let greeting = super.getGreeting();
+
+        if (this.hasHome()) {
+            greeting += ` I'm visiting from ${this.homeLocation}!`;
+        } else {
+            greeting;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Traveler('Lemuel Reyes', 'Vancouver');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
